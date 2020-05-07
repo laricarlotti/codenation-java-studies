e42cebee68362674078779272e7cf7cc900f6fab
LariCarlotti <larissa.carlotti@gmail.com>
2020-05-07 17:06:36 -0300

    First commit

---
+++ src/br/com/Main.java
@@ -0,0 +1,11 @@
+package br.com;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        String nome = "Larissa";
+
+        System.out.println("Hello " + nome);
+    }
+}
---
+++ src/br/com/variaveis/Primitivos.java
@@ -0,0 +1,25 @@
+package br.com.variaveis;
+
+public class Primitivos {
+
+    byte byteVar = 127;
+
+    short curto = 10000;
+
+    int inteiro = 100000000;
+
+    long longo = 10000000000000000L;
+
+
+
+    float pontoFlutuante = 10.10F;
+
+    double pontoFlutuanteDobro = 10000000000000.299;
+
+
+
+    char character = 'S';
+
+    boolean booleano = true;
+
+}
